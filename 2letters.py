a9084fe5c77f7af1b3791a7b45b27a744ec095d1
Nahnsu Dawkins <nahnsu@gmail.com>
2025-02-17 22:32:51 -0600

    b still messed up, but in a more interesting way that I want to preserve. Oh! and the AA intersection is working, got that squirreled away

--- 2letters.py
+++ 2letters.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 from solid import *
 from solid.utils import *
 import subprocess
@@ -26,60 +27,42 @@ def letter_A(h):
     solid_hole = linear_extrude(height=h)(positioned_hole)
     
     solid_with_hole = difference()(solid_A, solid_hole)
-    return solid_with_hole
+    centered = translate([-h/2, -h/2, -h/2])(solid_with_hole)
+    return centered
 
 def letter_B(h):
     large_arc = arc_points(h/3, -90, 90, num_points = 10, center=[2/3*h, 1/3*h])
-    # large_arc_transposed = translate([2/3*h, 1/3*h])(large_arc)
+    large_arc_shape = polygon(large_arc)
+    large_arc_transposed = translate([2/3*h, 1/3*h])(large_arc_shape)
 
     small_arc = arc_points(h/6, -90, 90, num_points= 10, center=[h/2, 2/3*h])
-    # small_arc_transposed = translate([h/2, 2/3*h])(small_arc)
+    small_arc_shape = polygon(small_arc)
+    small_arc_transposed = translate([h/2, 2/3*h])(small_arc_shape)
 
-    outer_points = [(0,0), (2/3*h, 0)]
-    outer_points.extend(large_arc)
-    outer_points.extend(small_arc)
-    outer_points.extend([(0,h)])
-
+    outer_points = [(0,0), (2/3*h, 0), (2/3*h, 2/3*h), (h/2, 2/3*h), (h/2, h), (0,h)]
     outer_shape = polygon(outer_points)
-    outer_solid = linear_extrude(height=h)(outer_shape)
+
+    total_shape = union()(large_arc_transposed, small_arc_transposed, outer_shape)
+    outer_solid = linear_extrude(height=h)(total_shape)
     return outer_solid
-
-
-def letter_3d(letter, size, font):
-    letter_2d = text(text=letter, size=size, font=font)
-
-    letter_3d = linear_extrude(height=size)(letter_2d)
-
-    return letter_3d
-
-def intersection_3d(letter1, letter2, size, font):
-    letter1_3d = letter_3d(letter1, size, font)
-    letter2_3d = letter_3d(letter2, size, font)
-
-    letter2_3d = rotate(a=90, v=[1,0,0])(letter2_3d)
-
-    intersect = intersection()(letter1_3d, letter2_3d)
-
-    return intersect
 
 
 
 if __name__ == "__main__":
-    # shapeA = letter_3d("A", 10, "Ariel")
-    # scad_render_to_file(shapeA, 'A.scad')
-    # shapeB = letter_3d("B", 10, "Ariel")
-    # shapeB = rotate(a=90, v=[1,0,0])(shapeB)
-    # final = intersection()(shapeA, shapeB)
-    # scad_render_to_file(final, "final.scad")
-    # # subprocess.run(["C:\\Program Files\\OpenSCAD\\openscad.exe", "-o", "A.stl", "A.scad"])
-    # # subprocess.run(["C:\\Program Files\\OpenSCAD\\openscad.exe", "-o", "B.stl", "B.scad"])
-    # subprocess.run(["C:\\Program Files\\OpenSCAD\\openscad.exe", "-o", "final.stl", "final.scad"])
 
+    # Just the letter A
     # a = letter_A(10)
     # scad_render_to_file(a, "customA.scad")
     # subprocess.run(["C:\\Program Files\\OpenSCAD\\openscad.exe", "-o", "customA.stl", "customA.scad"])
 
+    # Just the letter B
     b = letter_B(10)
     scad_render_to_file(b, "customB.scad")
     subprocess.run(["C:\\Program Files\\OpenSCAD\\openscad.exe", "-o", "customB.stl", "customB.scad"])
 
+    # Intersection of A and A
+    # a1 = translate([0,0,0])(letter_A(10))
+    # a2 = translate([0,0,0])(rotate(a=90, v=[0,1,0])(letter_A(10)))
+    # combined = intersection()(a1, a2)
+    # scad_render_to_file(combined, "AA.scad")
+    # subprocess.run(["C:\\Program Files\\OpenSCAD\\openscad.exe", "-o", "AA.stl", "AA.scad"])
